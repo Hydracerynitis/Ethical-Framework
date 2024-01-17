71125c56a01e6072327e4ab6106dab0db962ed49
Hydracerynitis <1578373943@qq.com>
2024-01-17 17:27:05 +1300

    Finalise Experiment Code, Upload for Server

--- Evaluate/multi_task.py
+++ Evaluate/multi_task.py
@@ -19,12 +19,29 @@ def generate_continuation_honest(pipeline,promts,tokenizer):
     display(f"{len(output)} numbers of continuation is generated with {multiprocessing.current_process().name}")
     return output
 
-def phi_generate(model,tokenizer,promts):
-    output=[]
-    for prom in promts:
-        inputs=tokenizer(prom,return_tensors="pt",return_attention_mask=False)
-        generation = model.generate(**inputs,max_length=200)
-        continuation = tokenizer.batch_decode(generation)[0]
-        output.append(continuation)
-    display(f"{len(output)} numbers of continuation is generated with {multiprocessing.current_process().name}")
-    return output
+
+def mixtral_generate(model,tokenizer,prompts,honest):
+    outputs=[]
+    for p in prompts:
+        inputs=tokenizer(p, return_tensors="pt")
+        if(honest):
+            output = model.generate(**inputs, max_length=len(tokenizer(p)['input_ids'])+10, num_return_sequences=20).to("cuda")
+        else:
+            output = model.generate(**inputs, max_new_tokens=40).to("cuda")
+        generation=tokenizer.decode(output[0], skip_special_tokens=True).replace(p,"")
+        outputs.append(generation)
+    print(f"{len(outputs)} numbers of continuation is generated with {multiprocessing.current_process().name}")
+    return outputs
+
+def phi_generate(model,tokenizer,promts,honest):
+    outputs=[]
+    for p in promts:
+        inputs=tokenizer(p, return_tensors="pt", return_attention_mask=False).to("cuda")
+        if(honest):
+            output = model.generate(**inputs, max_length=len(tokenizer(p)['input_ids'])+10, num_return_sequences=20).to("cuda")
+        else:
+            output = model.generate(**inputs, max_new_tokens=40).to("cuda")
+        generation = tokenizer.batch_decode(output)[0].replace(p,'')
+        outputs.append(generation)
+    display(f"{len(outputs)} numbers of continuation is generated with {multiprocessing.current_process().name}")
+    return outputs
